f1a2dff5b333339f8e2f98217a60052e51605085
baba <root@baba-mintcin.fritz.box>
2026-01-13 07:51:21 +0100

    Brushing up script

--- rclone/bin/decrypt-rclone-conf.sh
+++ rclone/bin/decrypt-rclone-conf.sh
@@ -5,12 +5,16 @@ cd ~/.dotfiles/rclone/.ssh || exit 1
 if [[ -f id_rclone.cpt ]]; then
   echo "Decrypting ssh-key ..."
   ccdecrypt id_rclone.cpt
+else
+  echo "No crypted file found!"
 fi
 
 cd ~/.dotfiles/rclone/.config/rclone/ || exit 1
 if [[ -f rclone.conf.cpt ]]; then
   echo "Decrypting rclone.conf ..."
   ccdecrypt rclone.conf.cpt
+else
+  echo "No crypted file found!"
 fi
 
 cd ~/.dotfiles || exit 1
